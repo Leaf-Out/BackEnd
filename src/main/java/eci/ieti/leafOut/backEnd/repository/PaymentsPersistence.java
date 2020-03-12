472d1b92d40cdb1199287f0d6c74398c5f407015
Sergio <sergio.rz.pz@gmail.com>
2020-03-12 09:15:56 -0500

    Change project structure to use CrudRepository :art:

---
+++ src/main/java/eci/ieti/leafOut/backEnd/repository/PaymentsPersistence.java
@@ -0,0 +1,17 @@
+package eci.ieti.leafOut.backEnd.persistence;
+
+/**
+ * This interface defines the methods used to persist a payment
+ *
+ * @author <a href=alejoguzm07@gmail.com> José Alejandro Naranjo Guzmán </a>
+ * @since 0.0.1
+ */
+public interface PaymentsPersistence {
+
+	void registerTransaction();
+	void getAllTransactions();
+	void getTransactionsByUserId();
+	void getTransactionById();
+	void updateTransaction();
+
+}
